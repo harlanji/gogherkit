2375a2ece318bc01cc3b640f6d38c52f8d837c7f
Harlan Iverson <h.ivrson@gmail.com>
2012-11-09 02:48:45 -0800

    moved login tests to samples directory

---
+++ samples/login/login_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "github.com/harlanji/gogherkit"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	ggk := new(gogherkit.GoGherKit)
+
+	var loginAPI *LoginAPI
+	var result bool
+
+	// BUG matchers with no tokens cause the system to fail
+	ggk.AddMatcher("Given", "the login system with no $stuff", func(params gogherkit.StepFuncParam) {
+
+		loginAPI = new(LoginAPI)
+		loginAPI.Init()
+
+	})
+
+	ggk.AddMatcher("When", "I login with username $username and password $password", func(params gogherkit.StepFuncParam) {
+		result = loginAPI.AttemptLogin(params["username"], params["password"])
+	})
+	ggk.AddMatcher("When", "I register with username $username and password $password", func(params gogherkit.StepFuncParam) {
+		result = loginAPI.Register(params["username"], params["password"])
+	})
+
+	ggk.AddMatcher("Then", "the result should be $bool", func(params gogherkit.StepFuncParam) {
+		if (result && params["bool"] != "true") || (!result && params["bool"] != "false") {
+			t.Error("Expected the result to be ", params["bool"], "actual", result)
+		}
+	})
+
+	ggk.RunFeatureFile("login.feature")
+}
---
+++ samples/login/main.go
@@ -0,0 +1,38 @@
+package main
+
+
+type LoginAPI struct {
+	credentials map[string]string
+}
+
+func (loginAPI *LoginAPI) Init() {
+	loginAPI.credentials = make(map[string]string, 100)
+}
+
+func (loginAPI *LoginAPI) Register(username string, password string) bool {
+	if _, present := loginAPI.credentials[username]; present {
+		return false
+	}
+
+	loginAPI.credentials[username] = password
+
+	return true
+}
+
+func (loginAPI *LoginAPI) ChangePassword(username string, newPassword string) bool {
+	if _, present := loginAPI.credentials[username]; !present {
+		return false
+	}
+
+	loginAPI.credentials[username] = newPassword
+
+	return true
+}
+
+func (loginAPI *LoginAPI) AttemptLogin(username string, password string) bool {
+	if pw, present := loginAPI.credentials[username]; !present || pw != password {
+		return false
+	}
+
+	return true
+}
